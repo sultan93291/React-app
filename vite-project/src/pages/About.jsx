40e5478b2c88f263e9c96f6e38b5a735725e806d
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2023-04-14 15:00:53 +0600

    Alhamdulillah

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// hook and jsx state and use state example
 
 function About() {
+  const [count, setCount] = useState(() => {
+    console.log("it's going good ");
+    return 0;
+  });
+
+  const [theme, setTheme] = useState(() => {
+    console.log('theme function is working there ');
+
+    return 'black';
+  });
+
+  function dcount() {
+    setCount(prevCount => prevCount - 1);
+    setTheme('Yellow');
+    //setCount(count - 1);
+  }
+
+  function icount() {
+    setCount(prevCount => prevCount + 1);
+    setTheme('Red');
+  }
+
   return (
-    <div>
-      <h1> hey this is sultan </h1>
+    <div className="m-2 fs-1">
+      <button onClick={dcount} className="btn btn-outline-danger m-2">
+        -
+      </button>
+
+      <span className="m-2 text-info">{count}</span>
+      <span className="m-2 text-info">{theme}</span>
+
+      <button onClick={icount} className="btn btn-outline-primary m-2 ">
+        {' '}
+        +{' '}
+      </button>
     </div>
   );
 }
